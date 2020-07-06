a4fb5fed0645d64df4fb46c703c2b0264a517835
Anton Klåvus <anton.klavus@iki.fi>
2020-07-06 11:00:19 +0300

    Added frontend

---
+++ build/precache-manifest.50e52863504f9742b31c17e9119d9c81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "722dd2371d974d1e7e46ed393dfeaa11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa356a675dbfe0bba9ab",
+    "url": "/static/js/2.4541878e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4541878e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "748a9a9a670873500867",
+    "url": "/static/js/main.18a72636.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
