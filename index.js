833b620087954fba30d93214a60f11f6875a6cbc
Anton Klåvus <anton.klavus@iki.fi>
2020-07-06 10:44:02 +0300

    Added Procfile and PORT env variable

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const cors = require("cors");
 
 app.use(express.json());
 morgan.token("body", (req, res) => {
@@ -13,6 +14,7 @@ app.use(
     ":method :url :status :res[content-length] - :response-time ms -  :body"
   )
 );
+app.use(cors());
 
 let persons = [
   {
@@ -93,7 +95,7 @@ app.post("/api/persons", (req, res) => {
   res.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
